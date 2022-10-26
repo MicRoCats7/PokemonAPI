d35182dee0ecd65294730c008affe3fa55314a36
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2022-10-26 21:23:30 +0700

    feat: [AI] change ui pokemonApi

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-fetch(' https://pokeapi.co/api/v2/pokemon')
-    .then(
-        function(response) {
-            if(response.status !== 200) {
-                console.log('Opps...' + response.status);
-                return;
-            }
+//get API pokemon
 
-            response.json().then(function(data){
-                const pokemon = data.results;
-                pokemon.forEach(pokemon => {
-                    document.getElementById('pokemonList')
-                    .insertAdjacentHTML('beforeend',
-                     `<li onclick='detail("${pokemon.url}")'>${pokemon.name}</li>`);
-                    
-                })
-            })
-        }
-    )
-    .catch(function(err) {
-        console.log('Fetch Error', err);
-    });
+const search_term = document.getElementById('search_q')
+const search_btn = document.getElementById('search-btn')
 
-    function detail(url){
-        fetch(url).then(function(response){
-            response.json().then(function(pokemon){
 
-                document.getElementById('card-detail').innerHTML = ""
-                
+// api https://pokeapi.co/docs/v2#pokemon
+const getPokemonData = async term => {
+    document.getElementById('show_error').classList.remove('show')
+    document.getElementById('show_error').classList.add('hidden')
+        
+    const url = `https://pokeapi.co/api/v2/pokemon/${term}`
+    const response = await fetch(url)
 
-                document.getElementById('card-detail')
-                .insertAdjacentHTML('beforeend',
-                 `<h3> ${pokemon.name}</h3>
-                  <img src="${pokemon.sprites.front_default}"/>
-                  <p>Jurus : ${pokemon.moves[0].move.name}</p>
-                  <p>Tinggi : ${pokemon.height}</p>
-                  <p>Berat :  ${pokemon.weight}</p>`
-                )          
-            })
-        })
-
+    if(response.status == 404 || response.statusText == 'Not Found'){
+        document.getElementById('show_error').classList.add('show')
+        document.getElementById('show_error').classList.remove('hidden')
+        return
     }
+
+    const pokemon = await response.json()
+    debugger
+
+    // update ui with data 
+    document.getElementById('update_img').setAttribute('src', pokemon.sprites.other.dream_world.front_default)
+    document.getElementById('update_name').innerHTML = pokemon.name
+    document.getElementById('update_candy_title').innerHTML = `${pokemon.name} Candy`
+    document.getElementById('update_hp').innerHTML = `HP ${Math.floor((Math.random() * pokemon.stats[0].base_stat) + 1)}/${pokemon.stats[0].base_stat}`
+    document.getElementById('update_cp').innerHTML = `XP ${pokemon.base_experience}`
+    document.getElementById('update_type').innerHTML = `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`
+    document.getElementById('update_weight').innerHTML = `${pokemon.weight}kg`
+    document.getElementById('update_height').innerHTML = `0.${pokemon.height}m`
+    document.getElementById('update_stardust').innerHTML = Math.floor((Math.random() * 10000) + 1)
+    document.getElementById('update_candy').innerHTML = Math.floor((Math.random() * 200) + 1)
+
+}
+
+search_btn.addEventListener('click', () => getPokemonData(search_term.value))
+//end pokemon api
